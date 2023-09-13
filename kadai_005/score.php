9e3cc85a8cb505d7d96633a370359e5438438be6
iwashi-yamashio <139742818+iwashi-yamashio@users.noreply.github.com>
2023-09-13 23:16:42 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_005</title>
+</head>
+
+<body>
+  <p>
+     <?php
+     // 変数に値を代入する
+     $user_score1 = 80;
+     $user_score2 = 60;
+     $user_score3 = 55;
+     $user_score4 = 40;
+     $user_score5 = 100;
+     $user_score6 = 25;
+     $user_score7 = 80;
+     $user_score8 = 95;
+     $user_score9 = 30;
+     $user_score10 = 60;
+
+      //平均点を出力する
+echo ($user_score1+$user_score2+$user_score3+$user_score4+$user_score5+$user_score6+$user_score7+$user_score8+$user_score9+$user_score10)/10
+
+     ?>
+  </p>
+</body>
+</html>
