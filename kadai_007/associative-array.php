675a7357a1940c101c419b50f03d4cfc1c793c94
iwashi-yamashio <139742818+iwashi-yamashio@users.noreply.github.com>
2023-09-17 00:22:42 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>kadai_007</title>
+</head>
+<body>
+  <?php
+//連装配列に値を代入する
+$array_data=['name'=>'onion',' price'=>'200','weight'=>'160',];
+
+//値を出力する
+print_r($array_data);
+  ?>
+</body>
+</html>
